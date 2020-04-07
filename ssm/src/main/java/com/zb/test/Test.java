c36c524245cc095e5771e769d8a532d4f2aee5fc
ghj <179891800@qq.com>
2020-04-07 23:48:09 +0800

    push ghj_dv分支上

--- ssm/src/main/java/com/zb/test/Test.java
+++ ssm/src/main/java/com/zb/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
     public static void main(String[] args) {
         //本地开发
         //别人提交的
+        //ghj开发了代码
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-mybatis.xml");
         UserService bean = context.getBean(UserService.class);
         User admin = bean.findUserLogin("admin", "123");

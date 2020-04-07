932bffc2053f0201fe50adb0ce9feb23861ce4ec
G19980215 <63275209+G19980215@users.noreply.github.com>
2020-04-07 23:42:34 +0800

    Update Test.java

--- ssm/src/main/java/com/zb/test/Test.java
+++ ssm/src/main/java/com/zb/test/Test.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         //本地开发
+        //别人提交的
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-mybatis.xml");
         UserService bean = context.getBean(UserService.class);
         User admin = bean.findUserLogin("admin", "123");
